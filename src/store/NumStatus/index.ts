8536140d2bd9895647720524b831a54090ef22ce
李阔 <1455749306@qq.com>
2022-11-02 17:01:18 +0800

    优化reducer

--- src/store/NumStatus/index.ts
+++ src/store/NumStatus/index.ts
@@ -11,6 +11,8 @@ export default {
         }
     },
     //名字统一管理
-    add1: "add1",
-    add2: "add2"
+    actionNames: {
+        add1: "add1",
+        add2: "add2"
+    }
 }
--- src/store/NumStatus/reducer.ts
+++ src/store/NumStatus/reducer.ts
@@ -7,15 +7,22 @@ let reducer = (state = {...handleNum.state }, action:{ type:string,
     
     let newState = JSON.parse(JSON.stringify(state))
 
-    switch(action.type) {
-        case handleNum.add1:
-            handleNum.actions["add1"](newState,action)
+    // switch(action.type) {
+    //     case handleNum.add1:
+    //         handleNum.actions["add1"](newState,action)
+    //         break;
+    //     case handleNum.add2:
+    //         handleNum.actions["add2"](newState,action)
+    //         break;
+    //     default:
+    //         break;
+    // }
+    // 【优化】
+    for(let key in handleNum.actionNames) {
+        if(action.type === handleNum.actionNames[key]){
+            handleNum.actions[handleNum.actionNames[key]](newState,action);
             break;
-        case handleNum.add2:
-            handleNum.actions["add2"](newState,action)
-            break;
-        default:
-            break;
+        }
     }
     return newState
 }
